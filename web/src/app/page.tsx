290e8615953d33e5323bde302e31620d89a32e2a
GusttavoHP <107812402+GusttavoHP@users.noreply.github.com>
2023-12-20 16:14:01 -0300

    add habit component in home

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from 'next/image'
+import Habit from '@/components/Habit'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
+      <Habit />
     </main>
   )
 }
