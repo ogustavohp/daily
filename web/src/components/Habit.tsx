8f3f89575a99e79ab5ea5baae7202ccb2a8ba60b
GusttavoHP <107812402+GusttavoHP@users.noreply.github.com>
2023-12-21 03:50:44 -0300

    remove habit component

--- web/src/components/Habit.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Habit() {
-  return (
-    <div className="bg-zinc-900 w-10 h-10 text-white rounded m-2 text-center flex items-center justify-center">
-      Habit
-    </div>
-  )
-}
