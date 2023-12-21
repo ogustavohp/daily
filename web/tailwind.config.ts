80b3ea15ee0affc74aabced745381c42262a46f4
GusttavoHP <107812402+GusttavoHP@users.noreply.github.com>
2023-12-21 03:49:17 -0300

    tailwind config grid rows

--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -8,10 +8,12 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        background: '#09090A',
+      },
+
+      gridTemplateRows: {
+        7: 'repeat(7, minmax(0,1fr))',
       },
     },
   },
