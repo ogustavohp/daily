daca78fc162475041188a5245a79e0fc6421ccea
GusttavoHP <107812402+GusttavoHP@users.noreply.github.com>
2023-12-21 18:13:02 -0300

    update ProgressBar component

---
+++ web/src/components/ProgressBar.tsx
@@ -0,0 +1,19 @@
+interface ProgressBarProps {
+  progress: number
+}
+
+export default function ProgressBar({ progress }: ProgressBarProps) {
+  return (
+    <div className="mt-4 h-3 w-full rounded-xl bg-zinc-700">
+      <div
+        role="progressbar"
+        aria-label="Progresso de hábitos completados nesse dia"
+        aria-valuenow={progress}
+        className="h-3 w-3/4 rounded-xl bg-violet-600"
+        style={{
+          width: `${progress}%`,
+        }}
+      />
+    </div>
+  )
+}
