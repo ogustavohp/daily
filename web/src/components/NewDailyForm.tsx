42ebfc52e25a6044d1aae54adbd1ceec2fdd9e77
GusttavoHP <107812402+GusttavoHP@users.noreply.github.com>
2023-12-22 00:28:43 -0300

    transition and style: NewDailyForm

--- web/src/components/NewDailyForm.tsx
+++ web/src/components/NewDailyForm.tsx
@@ -57,7 +57,7 @@ export default function NewDailyForm() {
         type="text"
         id="title"
         placeholder="Ex: Exercícios, dormir bem, etc..."
-        className="mt-3 rounded-lg bg-zinc-800 p-4 text-white placeholder:text-zinc-400"
+        className="mt-3 rounded-lg bg-zinc-800 p-4 text-white placeholder:text-zinc-400 focus:outline-none focus:ring-2 focus:ring-violet-600 focus:ring-offset-2 focus:ring-offset-zinc-900"
         autoFocus
         value={title}
         onChange={(e) => setTitle(e.target.value)}
@@ -71,11 +71,11 @@ export default function NewDailyForm() {
         {availableWeekDays.map((day, i) => (
           <Checkbox.Root
             key={`${day}-${i}`}
-            className="group flex items-center gap-3"
+            className="group flex items-center gap-3 focus:outline-none"
             checked={weekDays.includes(i)}
             onCheckedChange={() => handleToggleWeekDay(i)}
           >
-            <div className="flex h-8 w-8 items-center justify-center rounded-lg border-2 border-zinc-800 bg-zinc-900 group-data-[state=checked]:border-green-500 group-data-[state=checked]:bg-green-500">
+            <div className="flex h-8 w-8 items-center justify-center rounded-lg border-2 border-zinc-800 bg-zinc-900 transition-colors group-focus:ring-2 group-focus:ring-violet-600 group-focus:ring-offset-2 group-focus:ring-offset-background group-data-[state=checked]:border-green-500 group-data-[state=checked]:bg-green-500">
               <Checkbox.Indicator>
                 <Check size={20} className="text-white" />
               </Checkbox.Indicator>
@@ -88,7 +88,7 @@ export default function NewDailyForm() {
 
       <button
         type="submit"
-        className="mt-6 flex items-center justify-center gap-3 rounded-lg bg-green-600 p-4 hover:bg-green-500"
+        className="mt-6 flex items-center justify-center gap-3 rounded-lg bg-green-600 p-4 transition-colors hover:bg-green-500 focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2 focus:ring-offset-zinc-900"
       >
         <Check size={20} strokeWidth={2.5} />
         Confirmar
