b1ee75499f3ea78e95a9b487367d16d105c08bda
GusttavoHP <107812402+GusttavoHP@users.noreply.github.com>
2023-12-22 00:28:36 -0300

    transition and style: Header

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -13,7 +13,7 @@ export default function Header() {
       <Dialog.Root>
         <Dialog.Trigger
           type="button"
-          className="flex items-center gap-3 rounded-lg border border-violet-500 px-6 py-4 font-semibold hover:border-violet-300"
+          className="flex items-center gap-3 rounded-lg border border-violet-500 px-6 py-4 font-semibold transition-colors hover:border-violet-300 focus:outline-none focus:ring-2 focus:ring-violet-600 focus:ring-offset-2 focus:ring-offset-background"
         >
           <Plus size={20} className="text-violet-500" />
           New Daily
@@ -22,7 +22,7 @@ export default function Header() {
         <Dialog.Portal>
           <Dialog.Overlay className="fixed inset-0 h-screen w-screen bg-black/80" />
           <Dialog.Content className="absolute left-1/2 top-1/2 w-full max-w-md -translate-x-1/2 -translate-y-1/2 rounded-2xl bg-zinc-900 p-10">
-            <Dialog.Close className="absolute right-6 top-6 text-zinc-400 hover:text-zinc-200">
+            <Dialog.Close className="absolute right-6 top-6 rounded-lg text-zinc-400 hover:text-zinc-200 focus:outline-none focus:ring-2 focus:ring-violet-600 focus:ring-offset-2 focus:ring-offset-zinc-900">
               <X size={24} aria-label="Fechar" />
             </Dialog.Close>
             <Dialog.Title className="text-3xl font-extrabold leading-tight">
