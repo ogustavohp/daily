afd5ab451ea571e9b5fa0e832c8e6022c95707b6
GusttavoHP <107812402+GusttavoHP@users.noreply.github.com>
2023-12-21 23:12:25 -0300

    update server.ts for mobile suporte in localhost

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,15 +4,14 @@ import { appRoutes } from './routes'
 
 const app = fastify()
 
-app.register(cors, {
-  // origin: ['http://localhost:3000']
-})
+app.register(cors)
 
 app.register(appRoutes)
 
 app
   .listen({
     port: 3333,
+    host: '0.0.0.0',
   })
   .then(() => {
     console.log('Server running in: http://localhost:3333/')
