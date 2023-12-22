58787414afcba1e238e56d8c129ede1032a9aa85
GusttavoHP <107812402+GusttavoHP@users.noreply.github.com>
2023-12-21 22:09:06 -0300

    M: NewDailyForm

--- web/src/components/NewDailyForm.tsx
+++ web/src/components/NewDailyForm.tsx
@@ -1,8 +1,54 @@
 import { Check } from 'lucide-react'
+import * as Checkbox from '@radix-ui/react-checkbox'
+import { FormEvent, useState } from 'react'
+import { api } from '@/lib/axios'
+
+const availableWeekDays = [
+  'Domingo',
+  'Segunda-feira',
+  'Terça-feira',
+  'Quarta-feira',
+  'Quinta-feira',
+  'Sexta-feira',
+  'Sábado',
+]
 
 export default function NewDailyForm() {
+  const [title, setTitle] = useState('')
+  const [weekDays, setWeekDays] = useState<number[]>([])
+
+  function createNewHabit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+
+    if (!title || weekDays.length === 0) {
+      return
+    }
+
+    api
+      .post('/habits', {
+        title,
+        weekDays,
+      })
+      .then(() => {
+        alert('Daily criada com sucesso!')
+        setTitle('')
+        setWeekDays([])
+      })
+      .catch((err) => console.error(err))
+  }
+
+  function handleToggleWeekDay(weekDay: number) {
+    if (weekDays.includes(weekDay)) {
+      const weekDaysWithRemovedOne = weekDays.filter((day) => day !== weekDay)
+      setWeekDays(weekDaysWithRemovedOne)
+    } else {
+      const weekDaysWithAddedOne = [...weekDays, weekDay]
+      setWeekDays(weekDaysWithAddedOne)
+    }
+  }
+
   return (
-    <form className="mt-6 flex w-full flex-col">
+    <form onSubmit={createNewHabit} className="mt-6 flex w-full flex-col">
       <label htmlFor="title" className="font-semibold leading-tight">
         Qual seu comprometimento?
       </label>
@@ -10,14 +56,35 @@ export default function NewDailyForm() {
       <input
         type="text"
         id="title"
-        placeholder="ex: Exercícios, dormir bem, etc..."
+        placeholder="Ex: Exercícios, dormir bem, etc..."
         className="mt-3 rounded-lg bg-zinc-800 p-4 text-white placeholder:text-zinc-400"
         autoFocus
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
       />
 
       <label htmlFor="" className="mt-4 font-semibold leading-tight">
         Qual a recorrência
       </label>
+
+      <div className="mt-3 flex flex-col gap-2">
+        {availableWeekDays.map((day, i) => (
+          <Checkbox.Root
+            key={`${day}-${i}`}
+            className="group flex items-center gap-3"
+            checked={weekDays.includes(i)}
+            onCheckedChange={() => handleToggleWeekDay(i)}
+          >
+            <div className="flex h-8 w-8 items-center justify-center rounded-lg border-2 border-zinc-800 bg-zinc-900 group-data-[state=checked]:border-green-500 group-data-[state=checked]:bg-green-500">
+              <Checkbox.Indicator>
+                <Check size={20} className="text-white" />
+              </Checkbox.Indicator>
+            </div>
+
+            <span className="leading-tight text-white">{day}</span>
+          </Checkbox.Root>
+        ))}
+      </div>
 
       <button
         type="submit"
